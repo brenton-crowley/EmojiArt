10fda7bfa88dd8647c6462fd34d36708b3113124
Brent <brenton.crowley@gmail.com>
2022-07-08 16:37:18 +1000

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Brent on 8/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
